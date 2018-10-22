a8f7d64386283da9e1ecb4e9d55ce034b9236590
Xavier Yin <hendikoy@gmail.com>
2018-10-22 13:59:47 +0800

    release v1.0.0

---
+++ src/computed.js
@@ -0,0 +1,92 @@
+/*
+ * @Author: laixi 
+ * @Date: 2018-10-20 20:50:50 
+ * @Last Modified by: laixi
+ * @Last Modified time: 2018-10-21 22:41:50
+ */
+
+
+import { isObject, isFunction, result, isUpstream, pathToArray } from './utils'
+
+
+/**
+ * 初始化计算属性配置
+ */
+export function initializeComputed(computed) {
+  let data = [];
+  let key, value;
+  for (key in computed) {
+    value = computed[key];
+    if (isFunction(value)) {
+      data.push({ name: key, require: [], fn: value });
+    } else if (isObject(value)) {
+      let { require = [], fn } = value;
+      if (isFunction(fn)) {
+        data.push({ name: key, require, fn });
+      }
+    }
+  }
+  if (data.length > 1) {
+    data.sort((m, n) => {
+      if (~n.require.indexOf(m.name)) {
+        return -1;
+      } else if (~m.require.indexOf(n.name)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  return data;
+}
+
+export function evaluteComputed(ctx, changed, options) {
+  let { initial } = options || {};
+  let computedResult = {};
+  let computed = ctx.__computed;
+
+  let changedData;
+  if (computed.length) {
+    if (initial) {
+      for (let i in computed) {
+        let { fn, require: r, name } = computed[i];
+        changedData = r.reduce((memo, item) => {
+          let { key, value } = result(ctx.__data, item);
+          memo[item] = key ? value : result(computedResult, item).value;
+          return memo;
+        }, {});
+        computedResult[name] = fn.call(ctx, changedData);
+      }
+    } else {
+      let changedKeys = Object.keys(changed);
+      if (changedKeys.length) {
+        let pathCache = {};
+        let changedPaths = changedKeys.map(item => pathCache[item] = pathToArray(item));
+        for (let i in computed) {
+          let { fn, require: r, name } = computed[i];
+          if (r.length) {
+            let needUpdate = false;
+            let requiredName, requirePath;
+            for (let m in req) {
+              requiredName = req[m];
+              requirePath = pathCache[requiredName] || (pathCache[requiredName] = pathToArray(requiredName));
+              if (~changedPaths.findIndex(path => isUpstream(requirePath, path))) {
+                needUpdate = true;
+                break;
+              }
+            }
+            if (needUpdate) {
+              changedData = r.reduce((memo, item) => {
+                let { key, value } = result(ctx.__data, item);
+                memo[item] = key ? value : result(computedResult, item).value;
+                return memo;
+              }, {});
+              computedResult[name] = fn.call(ctx, changedData);
+            }
+          }
+        }
+      }
+    }
+  }
+  return computedResult;
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: laixi 
+ * @Date: 2018-10-20 12:56:52 
+ * @Last Modified by: laixi
+ * @Last Modified time: 2018-10-22 13:47:24
+ */
+import { pathPage } from './Page';
+import { pathComponent } from './Component'
+
+module.exports = {
+  pathComponent,
+  pathPage
+}
